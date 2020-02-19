6ebabbc101ffef64bb214ace9e053f5116f8a239
PinusThunbergii <aleksey.maksymyshyn@gmail.com>
2020-02-19 09:19:05 +0200

    255

--- chm3/Form1.Designer.cs
+++ chm3/Form1.Designer.cs
@@ -28,13 +28,128 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.mainChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.aTextBox = new System.Windows.Forms.TextBox();
+            this.bTextBox = new System.Windows.Forms.TextBox();
+            this.aLabel = new System.Windows.Forms.Label();
+            this.bLabel = new System.Windows.Forms.Label();
+            this.nLabel = new System.Windows.Forms.Label();
+            this.nTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.mainChart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(39, 361);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(75, 23);
+            this.calculateButton.TabIndex = 0;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButtonOnClickHandler);
+            // 
+            // mainChart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.mainChart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.mainChart.Legends.Add(legend1);
+            this.mainChart.Location = new System.Drawing.Point(361, 37);
+            this.mainChart.Name = "mainChart";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.mainChart.Series.Add(series1);
+            this.mainChart.Size = new System.Drawing.Size(609, 323);
+            this.mainChart.TabIndex = 1;
+            this.mainChart.Text = "chart1";
+            // 
+            // aTextBox
+            // 
+            this.aTextBox.Location = new System.Drawing.Point(114, 154);
+            this.aTextBox.Name = "aTextBox";
+            this.aTextBox.Size = new System.Drawing.Size(127, 20);
+            this.aTextBox.TabIndex = 2;
+            this.aTextBox.Text = "0";
+            // 
+            // bTextBox
+            // 
+            this.bTextBox.Location = new System.Drawing.Point(114, 192);
+            this.bTextBox.Name = "bTextBox";
+            this.bTextBox.Size = new System.Drawing.Size(127, 20);
+            this.bTextBox.TabIndex = 3;
+            this.bTextBox.Text = "1";
+            // 
+            // aLabel
+            // 
+            this.aLabel.AutoSize = true;
+            this.aLabel.Location = new System.Drawing.Point(38, 157);
+            this.aLabel.Name = "aLabel";
+            this.aLabel.Size = new System.Drawing.Size(13, 13);
+            this.aLabel.TabIndex = 4;
+            this.aLabel.Text = "a";
+            // 
+            // bLabel
+            // 
+            this.bLabel.AutoSize = true;
+            this.bLabel.Location = new System.Drawing.Point(38, 192);
+            this.bLabel.Name = "bLabel";
+            this.bLabel.Size = new System.Drawing.Size(13, 13);
+            this.bLabel.TabIndex = 5;
+            this.bLabel.Text = "b";
+            // 
+            // nLabel
+            // 
+            this.nLabel.AutoSize = true;
+            this.nLabel.Location = new System.Drawing.Point(38, 240);
+            this.nLabel.Name = "nLabel";
+            this.nLabel.Size = new System.Drawing.Size(13, 13);
+            this.nLabel.TabIndex = 7;
+            this.nLabel.Text = "n";
+            // 
+            // nTextBox
+            // 
+            this.nTextBox.Location = new System.Drawing.Point(114, 240);
+            this.nTextBox.Name = "nTextBox";
+            this.nTextBox.Size = new System.Drawing.Size(127, 20);
+            this.nTextBox.TabIndex = 6;
+            this.nTextBox.Text = "1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1074, 559);
+            this.Controls.Add(this.nLabel);
+            this.Controls.Add(this.nTextBox);
+            this.Controls.Add(this.bLabel);
+            this.Controls.Add(this.aLabel);
+            this.Controls.Add(this.bTextBox);
+            this.Controls.Add(this.aTextBox);
+            this.Controls.Add(this.mainChart);
+            this.Controls.Add(this.calculateButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.mainChart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.DataVisualization.Charting.Chart mainChart;
+        private System.Windows.Forms.TextBox aTextBox;
+        private System.Windows.Forms.TextBox bTextBox;
+        private System.Windows.Forms.Label aLabel;
+        private System.Windows.Forms.Label bLabel;
+        private System.Windows.Forms.Label nLabel;
+        private System.Windows.Forms.TextBox nTextBox;
     }
 }
 
--- chm3/Form1.cs
+++ chm3/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace chm3
 {
     public partial class Form1 : Form
@@ -15,6 +16,32 @@ namespace chm3
         public Form1()
         {
             InitializeComponent();
+        }
+        delegate double Function(double x);
+        double function(double x)
+        {
+            return Math.Cos(x) / (1.0d + Math.Pow(x, 2.0d));
+        }
+
+        double genRandDoubleInRange(double min, double max, ref Random rand)
+        {
+            double inMin = 0.0d, inMax = 1.0d;
+            return (rand.NextDouble() - inMin) * (max - min) / (inMax - inMin) + min ;
+        }
+
+        bool isInside(Function function, double a, double b, double n)
+        {
+
+            return false;
+        }
+
+        void calculateButtonOnClickHandler(object sender, EventArgs eventArgs)
+        {
+            Random rand = new Random();
+            for(int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(genRandDoubleInRange(20.0d, 40.0d, ref rand));
+            }
         }
     }
 }
